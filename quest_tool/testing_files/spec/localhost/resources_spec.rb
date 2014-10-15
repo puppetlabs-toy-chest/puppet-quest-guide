9dcd7994feac0171cfeee411af84bd84f51b658e
kjhenner <kjhenner@gmail.com>
2014-10-15 14:18:02 -0700

    Updated spec and task numbering for resources quest

--- quest_tool/testing_files/spec/localhost/resources_spec.rb
+++ quest_tool/testing_files/spec/localhost/resources_spec.rb
@@ -1,49 +1,25 @@
 require 'spec_helper'
 
-describe "The puppet describe command" do
-  it 'should be used to learn about the user resource type' do
-    file('/root/.bash_history').should contain "puppet describe user"
-  end
-end
-
 describe "The puppet resource command" do
   it 'should be used to inspect user root' do
     file('/root/.bash_history').should contain 'puppet resource user root'
   end
 end
 
-describe "user byte" do
-  it 'should be created' do
-    file('/root/.bash_history').should contain 'useradd byte'
+describe "The puppet describe command" do
+  it 'should be used to get a description of the user type' do
+    file('/root/.bash_history').should contain 'puppet describe user'
   end
 end
 
-describe "The puppet resource command" do
-  it 'should be used to inspect user byte' do
-    file('/root/.bash_history').should contain 'puppet resource user byte'
+describe "The user galatea" do
+  it 'should exist on the system' do
+    file('/etc/passwd').should contain "galatea"
   end
 end
 
-describe "user byte's password" do
-  it 'should be changed' do
-    file('/root/.bash_history').should contain 'passwd byte'
-  end
-end
-
-describe "The diretory /home/byte/tools" do
-  it "should be created" do
-    file('/root/.bash_history').should contain 'mkdir /home/byte/tools'
-  end
-end
-
-describe "user byte's password" do
-  it 'should be changed' do
-    file('/root/.bash_history').should contain 'passwd byte'
-  end
-end
-
-describe "The directory /home/byte/tools" do
-  it 'should be owned by byte' do
-    file('/root/.bash_history').should contain 'chown -R byte:byte /home/byte/tools'
+describe "The user galatea" do
+  it 'should have the comment Galatea of Cyprus' do
+    file('/etc/passwd').should contain 'Galatea of Cyprus'
   end
 end
