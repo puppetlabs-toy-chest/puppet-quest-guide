9a0689b5358a0cea05e77adf4f5accc4af404df7
kjhenner <kjhenner@gmail.com>
2014-10-16 16:30:44 -0700

    Added tests for mysql quest

---
+++ quest_tool/testing_files/spec/localhost/mysql_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+# Task 1
+describe "The Puppet Labs MySQL module" do
+  it 'should be installed' do
+    file('/etc/puppetlabs/puppet/modules/mysql').should be_directory
+    file('/etc/puppetlabs/puppet/modules/mysql/metadata.json').should contain 'puppetlabs-mysql'
+  end
+end
+
+# Task 2
+describe "The site.pp manifest" do
+  it 'should declare the mysql class' do
+    file('/etc/puppetlabs/puppet/manifests/site.pp').should contain "class { '::mysql::server':"
+    file('/etc/puppetlabs/puppet/manifests/site.pp').content.should match /\s*root_password\s+=>\s+/
+    file('/etc/puppetlabs/puppet/manifests/site.pp').content.should match /\s*override_options\s+=>\s+/
+  end
+end
+
+# Task 3
+describe "A MySQL server" do
+  it 'should be installed' do
+    file('/usr/bin/mysql').should be_file
+  end
+end
+
+# Task 4 
+describe "The mysql::server::account_security class" do
+  it "should be applied" do
+    shell("mysql -e 'show databases;'|grep test").exit_code.should_not be_zero
+  end
+end
+
+# Task 5
+describe "A database, user, and grant" do
+  it "should be created" do
+    shell("mysql -e 'show databases;'|grep lvm").exit_code.should be_zero
+    shell("mysql -e 'SELECT User FROM mysql.user;'|grep lvm_user").exit_code.should be_zero
+    shell("mysql -e 'show grants;'|grep lvm_user").exit_code.should be_zero
+  end
+end
