ee8802b1aeba24ab09993be3525da87dcbb91945
kjhenner <kjhenner@gmail.com>
2015-06-01 11:30:43 -0700

    Update spec tests for resource_ordering

--- quest_tool/.testing/spec/localhost/resource_ordering_spec.rb
+++ quest_tool/.testing/spec/localhost/resource_ordering_spec.rb
@@ -11,32 +11,56 @@ describe "Task 1:" do
 end
 
 describe "Task 2:" do
+  it 'Define the sshd class' do
+    file("#{MODULE_PATH}sshd/manifests/init.pp").content.should match /class sshd \{/
+    file("#{MODULE_PATH}sshd/manifests/init.pp").content.should match /package { 'openssh-server':/
+    file("#{MODULE_PATH}sshd/manifests/init.pp").content.should match /service { 'sshd':/
+  end
+end
+
+describe "Task 3:" do
+  it "Create a test manifest, and apply it with `--noop` and `--graph` flags" do
+    file("#{MODULE_PATH}sshd/tests/init.pp").content.should contain "include sshd"
+    file("/var/opt/bin/lib/pe-puppet/state/graphs/relationships.dot").content.should contain "sshd"
+  end
+end
+
+describe "Task 4:" do
+  it "Use the `dot` tool to generate an image of your resource relationships graph" do
+    file("/var/www/html/questguide/relationships.png").should be_file
+  end
+end
+
+describe "Task 5:" do
   it "Copy the sshd_config file to the module's files direcotry" do
     file("#{MODULE_PATH}sshd/files/sshd_config").should be_file
   end
 end
 
-describe "Task 3:" do
-  it 'Define the sshd class' do
-    file("#{MODULE_PATH}sshd/manifests/init.pp").content.should match /source\s=>\s\'puppet:\/\/\/modules\/sshd\/sshd_config\',/
-  end
-end
-
-describe "Task 4:" do
+describe "Task 6:" do
   it "Disable GSSAPIAuthentication in the module's sshd_config file" do
     file("#{MODULE_PATH}sshd/files/sshd_config").content.should match /^GSSAPIAuthentication no/
     file("#{MODULE_PATH}sshd/files/sshd_config").content.should_not match /^GSSAPIAuthentication yes/
   end
 end
 
-describe "Task 5:" do
-  it 'Include the sshd class in a test manifest' do
-    file("#{MODULE_PATH}sshd/tests/init.pp").should contain "sshd"
+describe "Task 7:" do
+  it 'Add a `file` resource to manage the `sshd` configuration file' do
+    file("#{MODULE_PATH}sshd/manifests/init.pp").should match /file\s+\{\s+'\/etc\/ssh\/sshd_config':/
+    file("#{MODULE_PATH}sshd/manifests/init.pp").should match /source\s+=>\s+'puppet:\/\/\/modules\/sshd\/sshd_config',/
+    file("#{MODULE_PATH}sshd/manifests/init.pp").should match /require\s+=>\s+'Package\['openssh-server'\]'/
   end
 end
 
-describe "Task 6:" do
-  it 'Add an openssh-server package declaration to your sshd class' do
-    file("#{MODULE_PATH}sshd/manifests/init.pp").should contain "package {"
+describe "Task 8:" do
+  it "Apply your test manifest with the `--noop` and `--graph` flags" do
+    file("/var/opt/bin/lib/pe-puppet/state/graphs/relationships.dot").content.should contain "sshd_config"
+  end
+end
+
+
+describe "Task 9:" do
+  it 'Add a `subscribe` metaparameter to your `sshd` resource' do
+    file("#{MODULE_PATH}sshd/manifests/init.pp").should match /subscribe\s+=>\s+File\['\/etc\/ssh\/sshd_config\]/
   end
 end
