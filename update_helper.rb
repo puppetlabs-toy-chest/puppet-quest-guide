2eb04b57f977ec1aff32d20033abb6a502cec6ee
kjhenner <kjhenner@gmail.com>
2015-12-18 10:22:00 -0800

    Cleaning up for git book

--- update_helper.rb
+++
@@ -1,8 +0,0 @@
-# Use bin and gems in /opt/quest to avoid conflicts with puppet's
-# bin and gems contents
-
-ENV['PATH'] +=  ':/opt/quest/bin'
-ENV['GEM_PATH'] = '/opt/quest/gems'
-
-# Currently does nothing, but we can later use this to add functionality
-# to the Rake update task without requiring a second run of the update tool.
