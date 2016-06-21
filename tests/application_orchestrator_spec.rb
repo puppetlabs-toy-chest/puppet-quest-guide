7e2e916b2155d15927ad07b3372ea861b4bf2f52
Trev Harmon <trevharmon@gmail.com>
2016-06-21 12:43:17 -0600

    Reflow syntax to increase readability
    There are a number of the tests that have particularly long lines.
    In order to increase readability, especially on limited width
    terminals, reflowed the code to follow a standard layout.

--- tests/application_orchestrator_spec.rb
+++ tests/application_orchestrator_spec.rb
@@ -9,8 +9,12 @@ end
 
 describe "Task 2:" do
   it "Run puppet on your nodes to apply your configuration changes" do
-    command('docker exec database puppet config print pluginsync use_cached_catalog --section agent').stdout.should match /pluginsync = false\suse_cached_catalog = true/
-    command('docker exec webserver puppet config print pluginsync use_cached_catalog --section agent').stdout.should match /pluginsync = false\suse_cached_catalog = true/
+    command('docker exec database puppet config print pluginsync use_cached_catalog --section agent')
+      .stdout
+      .should match /pluginsync = false\suse_cached_catalog = true/
+    command('docker exec webserver puppet config print pluginsync use_cached_catalog --section agent')
+      .stdout
+      .should match /pluginsync = false\suse_cached_catalog = true/
   end
 end
 
@@ -27,53 +31,65 @@ end
 
 describe "Task 4:" do
   it "Use the puppet access command to generate a token" do
-    file('/root/.puppetlabs/token').should be_file
+    file('/root/.puppetlabs/token')
+      .should be_file
   end
 end
 
 describe "Task 5:" do
   it "Create the directory structure of your lamp module" do
-    file("#{MODULE_PATH}lamp/manifests").should be_directory
-    file("#{MODULE_PATH}lamp/lib/puppet/type").should be_directory
+    file("#{MODULE_PATH}lamp/manifests")
+      .should be_directory
+    file("#{MODULE_PATH}lamp/lib/puppet/type")
+      .should be_directory
   end
 end
 
 describe "Task 6:" do
   it "Create a sql type" do
-    file("#{MODULE_PATH}lamp/lib/puppet/type/sql.rb").content.should match /Puppet::Type\.newtype :sql, :is_capability => true do/
+    file("#{MODULE_PATH}lamp/lib/puppet/type/sql.rb")
+      .content
+      .should match /Puppet::Type\.newtype :sql, :is_capability => true do/
   end
 end
 
 describe "Task 7:" do
   it "Define a lamp::mysql component" do
     # Need a better test!
-    file("#{MODULE_PATH}lamp/manifests/mysql.pp").should be_file
+    file("#{MODULE_PATH}lamp/manifests/mysql.pp")
+      .should be_file
   end
 end
 
 describe "Task 8:" do
   it "Define a lamp::webapp component" do
     # Need a better test!
-    file("#{MODULE_PATH}lamp/manifests/webapp.pp").should be_file
+    file("#{MODULE_PATH}lamp/manifests/webapp.pp")
+      .should be_file
   end
 end
 
 describe "Task 9:" do
   it "Define the lamp application" do
     # Need a better test!
-    file("#{MODULE_PATH}lamp/manifests/init.pp").should be_file
+    file("#{MODULE_PATH}lamp/manifests/init.pp")
+      .should be_file
   end
 end
 
 describe "Task 10:" do
   it "Declare an application instance in your site.pp manifest" do
     # Need a better test!
-    file("#{PROD_PATH}manifests/site.pp").content.should match /Node\['database\.learning\.puppetlabs\.vm'\]\s*=>\s*Lamp::Mysql\['app1'\],/
+    file("#{PROD_PATH}manifests/site.pp")
+      .content
+      .should match /Node\['database\.learning\.puppetlabs\.vm'\]\s*=>\s*Lamp::Mysql\['app1'\],/
   end
 end
 
 describe "Task 11:" do
   it "Trigger a puppet job run to deploy your application" do
-    command('docker exec webserver curl localhost/index.php').stdout.should match /successfully/
+    command('docker exec webserver curl localhost/index.php')
+      .stdout
+      .should match /successfully/
   end
 end
