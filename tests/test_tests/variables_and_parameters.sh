83f83c33d7d3f6b920e3c330028a8f96632ab89c
kjhenner <kjhenner@gmail.com>
2016-07-08 11:04:52 -0700

    fix variables_and_parameters test script

--- tests/test_tests/variables_and_parameters.sh
+++ tests/test_tests/variables_and_parameters.sh
@@ -7,12 +7,12 @@ mkdir -p web/{manifests,examples}
 cat << "EOL" > web/manifests/init.pp
 class web {
 
-   $doc_root = '/var/www/quest'
+  $doc_root = '/var/www/quest'
 
   $english = 'Hello world!'
   $french  = 'Bonjour le monde!'
 
-  file { "${doc_root}hello.html":
+  file { "${doc_root}/hello.html":
     ensure  => file,
     content => "<em>${english}</em>",
   }
@@ -37,17 +37,17 @@ class web ( $page_name, $message ) {
   $english = 'Hello world!'
   $french  = 'Bonjour le monde!'
 
-  file { "${doc_root}hello.html":
+  file { "${doc_root}/hello.html":
     ensure  => file,
     content => "<em>${english}</em>",
   }
 
-  file { "${doc_root}bonjour.html":
+  file { "${doc_root}/bonjour.html":
     ensure  => file,
     content => "<em>${french}</em>",
   }
 
-  file { "${doc_root}${page_name}.html":
+  file { "${doc_root}/${page_name}.html":
     ensure  => file,
     content => "<em>${message}</em>",
   }
