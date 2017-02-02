d728a70c1554847a60662b2c5785cc3631ed48b9
kjhenner <kjhenner@gmail.com>
2017-02-02 14:56:46 -0800

    update index.json, setup_helper.rb

--- tests/scripts/setup_helper.rb
+++ tests/scripts/setup_helper.rb
@@ -4,9 +4,24 @@ SCRIPT_DIR = File.dirname(__FILE__)
 
 $stdout.sync = true
 
-def create_node(name, image='agent')
+def create_node(name, image='agent', sign_cert=true, run_puppet=true)
   puts "Creating #{name}..."
-  `puppet apply -e "dockeragent::node { '#{name}': ensure => present, image => '#{image}' }"`
+  `puppet apply -e "dockeragent::node { '#{name}': ensure => present, image => '#{image}', privileged => true }"`
+  if sign_cert
+    wait_for_container(name)
+    `puppet cert sign #{name}`
+  end
+  if run_puppet
+    `docker exec #{name} puppet agent -t`
+  end
+end
+
+def wait_for_container(name)
+  count = 0
+  while !system("docker ps | grep #{name}") && count < 10 do
+    count =+ 1
+    sleep 2
+  end
 end
 
 def clear_nodes
