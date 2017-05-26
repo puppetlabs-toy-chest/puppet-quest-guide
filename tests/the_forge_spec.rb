79f73d5d69068a8ae58d9cae76ef4182abb9e8a9
Greg Larkin <greg.larkin@puppet.com>
2017-05-26 15:56:12 -0700

    Initial set of tests for the Forge quest

---
+++ tests/the_forge_spec.rb
@@ -0,0 +1,36 @@
+describe "Task 1:", host: :localhost do
+  it 'Install the PostgreSQL Puppet module' do
+    file("#{MODULE_PATH}postgresql")
+      .should be_directory
+    file('/root/.bash_history')
+      .content
+      .should match /puppet module list/
+  end
+end
+
+describe "Task 2:", host: :localhost do
+  it 'Create the pasture::db wrapper class' do
+    file("#{MODULE_PATH}pasture/manifests/db.pp")
+      .should be_file
+    file("#{MODULE_PATH}pasture/manifests/db.pp")
+      .content
+      .should match /class\s+pasture::db\s+\{.*?class\s+\{\s+(['"])postgresql::server\1:/m
+    command("puppet parser validate #{MODULE_PATH}pasture/manifests/db.pp")
+      .exit_status
+      .should be_zero
+  end
+end
+
+describe "Task 3:", host: :localhost do
+  it 'Classify the database node' do
+    file("#{PROD_PATH}manifests/site.pp")
+      .content
+      .should match /node\s+(['"])pasture\-db\.puppet\.vm\1\s+\{.*?include\s+pasture::db.*?\}/m
+    command("puppet parser validate #{PROD_PATH}manifests/site.pp")
+      .exit_status
+      .should be_zero
+    command('docker exec pasture-db.puppet.vm systemctl status postgresql.service | grep -q "active (running)"')
+      .exit_status
+      .should eq 0
+  end
+end
