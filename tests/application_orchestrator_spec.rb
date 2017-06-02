569bd142128d7566a7613344b16fa2d4eee02a28
Greg Larkin <greg.larkin@puppet.com>
2017-06-02 16:22:30 -0700

    Initial set of task tests for the App Orch quest

--- tests/application_orchestrator_spec.rb
+++ tests/application_orchestrator_spec.rb
@@ -1,55 +1,73 @@
-describe "Task 1:" do
-  it 'Create a node definition in your site.pp to configure your nodes for app deployment' do
-    # Ugly! Pending direct method to test manifests
-    file("#{PROD_PATH}manifests/site.pp")
-      .content
-      .should match /pe_ini_setting\s*{\s*['"]use_cached_catalog['"]\s*:
-                       .*
-                       setting\s*=>\s*['"]use_cached_catalog['"]\s*,
-                       .*
-                       pe_ini_setting\s*{\s*'pluginsync'\s*:
-                       .*
-                       setting\s*=>\s*'pluginsync',/mx
+describe 'Task 1:' do
+  it 'Create application module directories' do
+    file("#{MODULE_PATH}pasture_app/manifests")
+      .should be_directory
+    file("#{MODULE_PATH}pasture_app/lib/puppet/type")
+      .should be_directory
   end
 end
 
-describe "Task 2:" do
-  it "Run puppet on your nodes to apply your configuration changes" do
-    command('docker exec database puppet config print pluginsync use_cached_catalog --section agent')
-      .stdout
-      .should match /pluginsync\ =\ false\n
-                     use_cached_catalog\ =\ true/x
-    command('docker exec webserver puppet config print pluginsync use_cached_catalog --section agent')
-      .stdout
-      .should match /pluginsync\ =\ false\n
-                     use_cached_catalog\ =\ true/x
-  end
-end
-
-describe "Task 3:" do
-  it "Create a client configuration file" do
-    file("/root/.puppetlabs/client-tools/orchestrator.conf")
-      .content
-      .should match /"url":\s*"https:\/\/learning.puppetlabs.vm:8143"/
-    file("/root/.puppetlabs/client-tools/orchestrator.conf")
-      .content
-      .should match /"environment":\s*"production"/
-  end
-end
-
-describe "Task 4:" do
-  it "Use the puppet access command to generate a token" do
-    file('/root/.puppetlabs/token')
+describe 'Task 2:' do
+  it 'Create sql resource type file' do
+    file("#{MODULE_PATH}pasture_app/lib/puppet/type/sql.rb")
       .should be_file
+    command("ruby -c #{MODULE_PATH}pasture_app/lib/puppet/type/sql.rb >/dev/null 2>&1")
+      .exit_status
+      .should eq 0
+    file("#{MODULE_PATH}pasture_app/lib/puppet/type/sql.rb")
+      .content
+      .should match /Puppet::Type\.newtype\s+:sql,\s+:is_capability\s+=>\s+true\s+do\n(\s\snewparam\s+:(\w)+(,\s+:is_namevar\s+=>\s+(true|false))?\n)+^end$/m
   end
 end
 
-describe "Task 5:" do
-  it "Create the directory structure of your lamp module" do
-    file("#{MODULE_PATH}lamp/manifests")
-      .should be_directory
-    file("#{MODULE_PATH}lamp/lib/puppet/type")
-      .should be_directory
+describe 'Task 3:' do
+  it 'Create the pasture_app::db component' do
+    file("#{MODULE_PATH}pasture_app/manifests/db.pp")
+      .should be_file
+    command("puppet parser validate --app_management #{MODULE_PATH}pasture_app/manifests/db.pp")
+      .exit_status
+      .should be_zero
+    file("#{MODULE_PATH}pasture_app/manifests/db.pp")
+      .content
+      .should match /define\s+pasture_app::db\s+\(.*?\$user,.*?\$password,.*?\$host\s+=\s+\$::fqdn,.*?/m
+    file("#{MODULE_PATH}pasture_app/manifests/db.pp")
+      .content
+      .should match /Pasture_App::Db\s+produces\s+Sql\s+{.*?user\s+=>\s+\$user,/m
+  end
+end
+
+describe 'Task 4:' do
+  it 'Create the pasture_app::app component' do
+    file("#{MODULE_PATH}pasture_app/manifests/app.pp")
+      .should be_file
+    command("puppet parser validate --app_management #{MODULE_PATH}pasture_app/manifests/app.pp")
+      .exit_status
+      .should be_zero
+    file("#{MODULE_PATH}pasture_app/manifests/app.pp")
+      .content
+      .should match /define\s+pasture_app::app\s+\(.*?\$db_user,.*?\$db_password,.*?\$db_host,.*?/m
+    file("#{MODULE_PATH}pasture_app/manifests/app.pp")
+      .content
+      .should match /Pasture_App::App\s+consumes\s+Sql\s+{.*?db_user\s+=>\s+\$user,/m
+  end
+end
+
+describe 'Task 5:' do
+  it 'Create the pasture_app application class' do
+    file("#{MODULE_PATH}pasture_app/manifests/init.pp")
+      .should be_file
+    command("puppet parser validate --app_management #{MODULE_PATH}pasture_app/manifests/init.pp")
+      .exit_status
+      .should be_zero
+    file("#{MODULE_PATH}pasture_app/manifests/init.pp")
+      .content
+      .should match /application\s+pasture_app\s+\(.*?\$db_user,.*?\$db_password,.*?/m
+    file("#{MODULE_PATH}pasture_app/manifests/init.pp")
+      .content
+      .should match /pasture_app::db\s+{\s+\$name:.*?export\s+=>\s+Sql\[\$name\],/m
+    file("#{MODULE_PATH}pasture_app/manifests/init.pp")
+      .content
+      .should match /pasture_app::app\s+{\s+\$name:.*?consume\s+=>\s+Sql\[\$name\],/m
   end
 end
 
