3f746fe96480a10177b24270edb6e0f9ebedd04b
kjhenner <kjhenner@gmail.com>
2017-07-06 10:47:50 -0700

    Clean up unused files in repo

--- strip_tag.rb
+++
@@ -1,6 +0,0 @@
-Dir.glob('./quests/*.md').each do |f|
-  text = File.read(f)
-  text.gsub!(/{% task (\d+) %}.+?{% endtask %}/m, '<div class = "lvm-task-number"><p>Task \1:</p></div>')
-  File.write(f, text)
-end
-
