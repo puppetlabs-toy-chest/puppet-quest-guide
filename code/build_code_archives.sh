5051b88ce538c33451ace67e24df7c8e909f16d4
Greg Larkin <greg.larkin@puppet.com>
2019-01-16 11:53:05 -0800

    Updated archive build script to remove leading path elements

--- code/build_code_archives.sh
+++ code/build_code_archives.sh
@@ -2,4 +2,4 @@
 
 # For each code directory, build a code archive that can be copied to the
 # learning VM master
-find . -type d -mindepth 1 -maxdepth 1 | cut -f2 -d/ | xargs -n1 -I % sh -c "tar cvf - % | gzip -9v > %.tar.gz"
+find . -type d -mindepth 1 -maxdepth 1 | cut -f2 -d/ | xargs -n1 -I % sh -c "tar -C % -cvf - . | gzip -9v > %.tar.gz"
