0e9a7b92ef7b3adcc291d18b5a3e3584f2bfa845
Greg Larkin <greg.larkin@puppet.com>
2019-02-04 17:15:46 -0800

    Add tests for task #2 of the bolt quest

--- tests/hello_bolt_spec.rb
+++ tests/hello_bolt_spec.rb
@@ -24,3 +24,24 @@ describe _("Task 1:") do
       .should be_installed
   end
 end
+
+describe _("Task 2:") do
+  it 'has a working solution', :solution do
+    command('bolt --help')
+      .exit_status
+      .should eq 0
+    command('bolt --version')
+      .exit_status
+      .should eq 0
+  end
+  it _('View the options for the bolt tool'), :validation do
+    file('/root/.bash_history')
+      .content
+      .should match /bolt\s+(-h|--help)/
+  end
+  it _('View the version for the bolt tool'), :validation do
+    file('/root/.bash_history')
+      .content
+      .should match /bolt\s+\-\-version/
+  end
+end
